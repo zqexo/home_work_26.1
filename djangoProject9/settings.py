6bd51c42f18c9e46c071254f22106313b147bb24
Maksim Netesov <MrEasters@mail.ru>
2024-11-02 17:12:40 +0300

    add celery

--- djangoProject9/settings.py
+++ djangoProject9/settings.py
@@ -132,3 +132,18 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
+
+# URL-адрес брокера сообщений
+CELERY_BROKER_URL = 'redis://localhost:6379'  # Например, Redis, который по умолчанию работает на порту 6379
+
+# URL-адрес брокера результатов, также Redis
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+# Часовой пояс для работы Celery
+CELERY_TIMEZONE = "Australia/Tasmania"
+
+# Флаг отслеживания выполнения задач
+CELERY_TASK_TRACK_STARTED = True
+
+# Максимальное время на выполнение задачи
+CELERY_TASK_TIME_LIMIT = 30 * 60
